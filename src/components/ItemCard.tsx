0aac961786a8a87d23babcd2a06cdbe0055ba5ec
Enoch <enoch@cydef.ca>
2024-04-24 16:38:00 -0400

    minor - styling

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -14,13 +14,13 @@ export default function ItemCard({ item }: Props) {
 
   return (
     <li
-      className='bg-white flex flex-col justify-center items-center rounded-2xl p-5 text-black shadow-2xl'
+      className='bg-white flex flex-col justify-center items-center rounded-2xl p-5 text-black shadow-2xl cursor-pointer hover:opacity-80'
       onClick={handleOnClick}
     >
       <img
         src={item.imageSrc}
         alt='Item image'
-        className='rounded-lg w-[60%] -mt-12 '
+        className='rounded-lg w-[60%] -mt-12 shadow-lg'
       />
       <p className='text-3xl font-bold mt-6'>{item.name}</p>
       <div className='h-1.5 w-28 bg-gray-300 my-6 rounded-b-full'></div>
